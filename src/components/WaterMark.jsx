d64b1b713526f117f1d7e99b7df8be76b0cdd5e5
GabrielJesus0 <gabriel053jesus@gmail.com>
2025-07-16 15:19:24 -0300

    Atualização  WaterMark

--- src/components/WaterMark.jsx
+++ src/components/WaterMark.jsx
@@ -5,7 +5,7 @@ function WaterMark() {
     <a
       href="https://github.com/gabrieljesuscode"
       target="_blank"
-      className="py-2 px-3 rounded-lg text-white fixed bottom-12 flex gap-2 bg-slate-700 xl:right-10 active:scale-95 transition"
+      className="py-2 px-3 rounded-lg text-white fixed bottom-12 flex gap-2 items-center bg-slate-700 xl:right-10 active:scale-95 transition"
     >
       Visite meu github <strong>gabrieljesuscode</strong>
       <img src={github} alt="Logo do GitHub" className="w-6 h-6 invert" />
